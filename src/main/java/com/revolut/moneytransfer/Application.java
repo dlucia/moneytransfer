c2cbeb64b9e66ea6aaa77da30f7d0153a342d21c
Donato Lucia <donato.lucia@lastminute.com>
2017-12-28 20:32:41 +0100

    Project setup with Jersey

---
+++ src/main/java/com/revolut/moneytransfer/Application.java
@@ -0,0 +1,30 @@
+package com.revolut.moneytransfer;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
+import static org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory.createHttpServer;
+
+public class Application
+{
+  public static final String BASE_URI = "http://localhost:8080/revolut/";
+  private static final String RESOURCE_PACKAGE = "com.revolut.moneytransfer.api";
+
+  public static void main(String[] args)
+  {
+    startServer();
+  }
+
+  public static HttpServer startServer()
+  {
+    return createHttpServer(URI.create(BASE_URI),
+                            new ResourceConfig()
+                                .packages(RESOURCE_PACKAGE));
+  }
+
+}
+
+
+
